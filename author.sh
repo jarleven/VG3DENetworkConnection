64d1758c214fd786c823885903fcdc52dd8b6fcb
Augustin <augustin.winther@protonmail.com>
2018-08-31 10:12:44 +0200

    Added TODO, Status and link

--- author.sh
+++ author.sh
@@ -10,22 +10,24 @@ sed -i '/Autotable start/G' README.md
 # Loop all files, extract filename and author put in after matching pattern
 for filename in ./Panels/*.md; do
 	echo $filename
-	b=$(basename $filename .md)
+	panel=$(basename $filename .md)
 	#echo $b
 	#cat $filename | grep "Author"
 	author=$(cat $filename | grep "Author" | sed -e 's/##### Author: //g')
+	todo=$(cat $filename | grep "TODO" | sed -e 's/##### TODO: //g')
+	status=$(cat $filename | grep "Tilstand" | sed -e 's/##### Tilstand: //g')
 	
 	# Build the table row
-	tabell=$(echo "|"$b"|"$author"|")
+	tabell=$(echo "|""["$panel"]""(Panels/"$panel".md)""|"$author"|"$todo"|"$status"|")
 	
 	# Add the row after the matching table
 	sed -i "/Autotable start/a $tabell" README.md
 done
 
 # Add the table header
-tabell=$(echo "|----|----|")
+tabell=$(echo "|----|----|----|----|")
 sed -i "/Autotable start/a $tabell" README.md
-tabell=$(echo "|Patch|Coordinator|")
+tabell=$(echo "|Patch|Coordinator|TODO|Status|")
 sed -i "/Autotable start/a $tabell" README.md
 
 # Add a blank line at the top
