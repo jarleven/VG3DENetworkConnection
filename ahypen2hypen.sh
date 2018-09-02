f2a73ebbc2d85a5109c634c39246a5c0702aeab0
jarleven <jarleven@gmail.com>
2018-09-02 15:54:32 +0200

    Update ahypen2hypen.sh
    Some documentation of the function

--- ahypen2hypen.sh
+++ ahypen2hypen.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-## Loops through files in panels
+### Remove the door name from the documentation DE2101A --> DE2101
+##
+## We have the following patterns I believe.
+##  [2101A-]
+##  [2010A -]
+##  [2101A.] 
+
+## A full stop (.) we need to escape like this (\.) or sed will interpret it as a command.
+
+##  Following Git command comes handy when playing with this.
+## Just remember to keep what you have in this file.
+## git reset --hard HEAD
+
+## Loops through files in the Panels folder
 for filename in ./Panels/*.md
 do
 	# [0-9] regular expression. [0-9][0-9][0-9][0-9] will match all numbers 0000 to 9999
@@ -10,7 +23,6 @@ do
 	sed -i -e '/[0-9][0-9][0-9][0-9]A/s/A-/-/g' $filename
 	sed -i -e '/[0-9][0-9][0-9][0-9]A/s/A\./\./g' $filename
 
-	# Comes handy when playing with this. Just remember to keep what you have in this file
-	# git reset --hard HEAD
+
 done
 
