58f9be77426eac23e22d41a6cb86e4ec392e3c01
Jarl Even Englund <jarleven@gmail.com>
2018-09-02 10:19:06 +0200

    For the DE2101A to DE2101 rename

--- ahypen2hypen.sh
+++ ahypen2hypen.sh
@@ -3,7 +3,14 @@
 ## Loops through files in panels
 for filename in ./Panels/*.md
 do
-	sed -i -e 's/A-/-/g' $filename
-	sed -i -e 's/A./-/g' $filename
+	# [0-9] regular expression. [0-9][0-9][0-9][0-9] will match all numbers 0000 to 9999
+	# . need to be escaped \.
+	# /OnlyLinesWith/s/WhatToReplace/ReplaceWith/
+	sed -i -e '/[0-9][0-9][0-9][0-9]A/s/A -/ -/g' $filename
+	sed -i -e '/[0-9][0-9][0-9][0-9]A/s/A-/-/g' $filename
+	sed -i -e '/[0-9][0-9][0-9][0-9]A/s/A\./\./g' $filename
+
+	# Comes handy when playing with this. Just remember to keep what you have in this file
+	# git reset --hard HEAD
 done
 
