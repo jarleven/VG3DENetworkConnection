ab5f6cc5e845721ab5768275c6793b9e748e1802
Jarl Even Englund <jarleven@gmail.com>
2018-09-02 09:52:37 +0200

    For the DE2101A to DE2101 rename

--- ahypen2hypen.sh
+++ ahypen2hypen.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
 ## Loops through files in panels
 for filename in ./Panels/*.md
 do
-
-		sed -i -e 's/A-/-/g' $filename
-
+	sed -i -e 's/A-/-/g' $filename
+	sed -i -e 's/A./-/g' $filename
 done
 
