3fa49bf2af166a81ddf55af9b842be013e071002
Jarl Even Englund <jarleven@gmail.com>
2020-01-30 15:37:41 -0800

    Build doc issues

--- installDoc.sh
+++ installDoc.sh
@@ -3,5 +3,6 @@
 sudo apt install -y texlive-full
 sudo apt install -y pandoc
 sudo apt install -y pdftk
-sudo apt install -y texlive-latex-base
+#sudo apt install -y texlive-latex-base
+sudo apt install -y texlive
 
